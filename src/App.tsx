0930820c36a0367076ecfa7904b50eba0a1eb5e7
pedro-almeida97 <pedro.almeidah@ufu.br>
2025-11-14 18:58:19 -0300

    componente  dica

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styles from './app.module.css'
 
 import { Header } from './components/Header'
+import { Tip } from './components/Tip'
 
 export default function App() {
   function handleRestart() {
@@ -11,6 +12,7 @@ export default function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestart} />
+        <Tip tip="Biblioteca para criar interfaces Web com Javascript." />
       </main>
     </div>
   )
